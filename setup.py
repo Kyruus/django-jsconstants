ed04d4ce50394a3e71afa0583b934484101f0173
Paavan Bhavsar <pbhavsar@kyru.us>
2013-06-27 13:59:17 -0400

    Fixed manifest and setup.py to work with python packaging.

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import os
 from distutils.core import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-jsconstants',
